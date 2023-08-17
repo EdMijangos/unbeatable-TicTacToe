d2c7c2bbe4b5a11ea48683e10558f08a15bb8ab9
Jose Loeza <mloeza2010@hotmail.com>
2023-08-17 21:03:41 +0900

    made it unbeatable

--- player.py
+++ player.py
@@ -17,9 +17,51 @@ class ComputerPlayer(Player):
 
     def get_move(self, game):
         time.sleep(1) # break to improve UX flow
-        move = random.choice(game.available_moves())
+        if len(game.available_moves()) == 9:
+            move = random.choice(game.available_moves())
+        else:
+            move = self.minimax(game, self.letter)['move']
         print(f'AI moved to {move}')
         return move
+    
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        if state.winner == other_player:
+            score = 1 * (len(state.available_moves()) + 1) if other_player == max_player else -1 * (len(state.available_moves()) + 1)
+            return {'move': None, 'score': score}
+        elif len(state.available_moves()) == 0:
+            return {'move': None, 'score': 0}
+        
+        if player == max_player:
+            best = {'score': -float('inf')}
+        else:
+            best = {'score': float('inf')}
+
+        for move in state.available_moves():
+            # try all possible moves
+            state.make_move(move, player)
+
+            # recurse minimax with new state
+            sim_score = self.minimax(state, other_player)
+
+            # undo changes
+            state.board[move] = ' '
+            state.winner = None 
+
+            # save move that was tried
+            sim_score['move'] = move
+
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+
+        return best
+
 
 
 class HumanPlayer(Player):
